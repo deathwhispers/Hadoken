08bc9294db5edc5d09447b2bf496955d606d72ce
杨光剑 <yangguangjian@rhytech.com.cn>
2022-03-01 09:18:10 +0800

    add some modules to project

---
+++ hadoken-infra/hadoken-infra-web/src/main/java/com/guangjian/hadoken/infra/web/core/handler/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.guangjian.hadoken.infra.web.core.handler;
+
+/**
+ * @Author: yanggj
+ * @Description: TODO
+ * @Date: 2022/03/01 9:05
+ * @Version: 1.0.0
+ */
+public class GlobalExceptionHandler {
+}
---
+++ hadoken-infra/hadoken-infra-web/src/main/java/com/guangjian/hadoken/infra/web/core/util/WebFrameworkUtils.java
@@ -0,0 +1,85 @@
+package com.guangjian.hadoken.infra.web.core.util;
+
+import com.guangjian.hadoken.common.result.CommonResult;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Author: yanggj
+ * @Description: web 包的工具类
+ * @Date: 2022/03/01 9:05
+ * @Version: 1.0.0
+ */
+public class WebFrameworkUtils {
+
+    private static final String REQUEST_ATTRIBUTE_LOGIN_USER_ID = "login_user_id";
+    private static final String REQUEST_ATTRIBUTE_LOGIN_USER_TYPE = "login_user_type";
+
+    private static final String REQUEST_ATTRIBUTE_COMMON_RESULT = "common_result";
+
+    public static void setLoginUserId(ServletRequest request, Long userId) {
+        request.setAttribute(REQUEST_ATTRIBUTE_LOGIN_USER_ID, userId);
+    }
+
+    public static void setLoginUserType(ServletRequest request, Integer userType) {
+        request.setAttribute(REQUEST_ATTRIBUTE_LOGIN_USER_TYPE, userType);
+    }
+
+    /**
+     * 获得当前用户的编号，从请求中
+     *
+     * @param request 请求
+     * @return 用户编号
+     */
+    public static Long getLoginUserId(HttpServletRequest request) {
+        if (request == null) {
+            return null;
+        }
+        return (Long) request.getAttribute(REQUEST_ATTRIBUTE_LOGIN_USER_ID);
+    }
+
+    /**
+     * 获得当前用户的类型，从请求中
+     *
+     * @param request 请求
+     * @return 用户编号
+     */
+    public static Integer getLoginUserType(HttpServletRequest request) {
+        if (request == null) {
+            return null;
+        }
+        return (Integer) request.getAttribute(REQUEST_ATTRIBUTE_LOGIN_USER_TYPE);
+    }
+
+    public static Integer getLoginUserType() {
+        HttpServletRequest request = getRequest();
+        return getLoginUserType(request);
+    }
+
+    public static Long getLoginUserId() {
+        HttpServletRequest request = getRequest();
+        return getLoginUserId(request);
+    }
+
+    public static void setCommonResult(ServletRequest request, CommonResult<?> result) {
+        request.setAttribute(REQUEST_ATTRIBUTE_COMMON_RESULT, result);
+    }
+
+    public static CommonResult<?> getCommonResult(ServletRequest request) {
+        return (CommonResult<?>) request.getAttribute(REQUEST_ATTRIBUTE_COMMON_RESULT);
+    }
+
+    private static HttpServletRequest getRequest() {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (!(requestAttributes instanceof ServletRequestAttributes)) {
+            return null;
+        }
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) requestAttributes;
+        return servletRequestAttributes.getRequest();
+    }
+
+}
